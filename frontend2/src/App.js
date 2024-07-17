cd6f56b8cfc7c13413ae1d6b55cebce5e238eba6
mwegter95 <mwegter95@gmail.com>
2024-07-17 14:45:34 -0500

    nice navbar first draft

---
+++ frontend2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TopNavbar from "./components/Navbar";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavbar />
+      {/* Other components go here */}
+    </div>
+  );
+}
+
+export default App;

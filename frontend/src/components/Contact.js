6b132fd2aa74db266cfb6f50b141f8b2d5beeb29
mwegter95 <mwegter95@gmail.com>
2024-07-25 20:33:18 -0500

    nice contact page too

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -4,28 +4,29 @@ import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <Container className="contact-page">
-      <h1>Contact Us</h1>
-      <div className="contact-card">
-        <Card className="business-card">
-          <Card.Body>
-            <Card.Title>For The Record</Card.Title>
-            <Card.Text>
-              <strong>Email:</strong> michael@fortherecordmn.com
-              <br />
-              <strong>Phone:</strong> 763-496-7006
-              <br />
-              <strong>Note:</strong> Please call or text, and leave a message
-              with your wedding date. I'll call back within 1-2 business days.
-              <br />
-            </Card.Text>
-            <Button variant="primary" href="/booknow">
-              Book Now!
-            </Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </Container>
+    <div className="contact-page">
+      <Container>
+        <h1>Contact Us</h1>
+        <div className="contact-card">
+          <Card className="business-card">
+            <Card.Body>
+              <Card.Title>For The Record</Card.Title>
+              <Card.Text>
+                <strong>Email:</strong> michael@fortherecordmn.com
+                <br />
+                <strong>Phone:</strong> 763-496-7006
+                <br />
+                Please call or text, leave a message with your wedding date.
+                I'll call back within 1-2 business days.
+              </Card.Text>
+              <Button variant="primary" href="/booknow">
+                Book Now!
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+    </div>
   );
 };
 

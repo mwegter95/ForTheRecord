5dc8103ae7eb6aa1e283a92091846c80a681b8a0
mwegter95 <mwegter95@gmail.com>
2024-07-25 20:18:46 -0500

    nice about us page best

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -8,44 +8,43 @@ import DJDecks from "../images/DJ_Decks.jpeg";
 const About = () => {
   return (
     <div className="about-page">
-      <Container className="about-container">
-        <h1>About Us</h1>
-        <div className="welcome-text">
-          <h2>Welcome to For the Record</h2>
-          <p>
-            Welcome to For the Record! We are dedicated to providing the best DJ
-            and audio experience for your events. Learn more about our journey,
-            and what makes us unique.
-          </p>
-          <h2>WHY US?</h2>
-          <p>
-            For The Record is all about making sure your event's audio is
-            sounding great and has no hiccups. Founded by Michael "DJ Synfinity"
-            Wegter with the intention to root out bad audio at events, For The
-            Record is dedicated to using passion and experience to provide great
-            service, with a great sound, at a great rate.
-          </p>
+      <Container>
+        <h1 className="about-us-heading">About Us</h1>
+        <div className="about-header">
+          <div className="about-header-content">
+            <h2>Welcome to For the Record!</h2>
+            <p>
+              We are dedicated to providing the best DJ and audio experience for
+              your events. Learn more about our journey, and what makes us
+              unique.
+            </p>
+            <h2>WHY US?</h2>
+            <p>
+              For The Record is all about making sure your event's audio is
+              sounding great and has no hiccups. Founded by Michael "DJ
+              Synfinity" Wegter with the intention to root out bad audio at
+              events, For The Record is dedicated to using passion and
+              experience to provide great service, with a great sound, at a
+              great rate.
+            </p>
+          </div>
+          <img
+            src={PortraitMe}
+            alt="Michael Wegter"
+            className="about-header-image"
+          />
         </div>
-        <Row className="about-row">
-          <Col md={6} className="about-card-image">
-            <img src={PortraitMe} alt="Our Team" />
-          </Col>
-          <Col md={6} className="about-card-text">
+        <Row className="justify-content-center">
+          <Col md={8}>
             <Card className="about-card">
               <Card.Body>
                 <Card.Title>Our Team</Card.Title>
                 <Card.Text>
-                  Meet the people who make the magic happen. Here at For The
-                  Record, we're a team of one! Michael is an experienced DJ and
-                  event audio planner dedicated to making your event
-                  unforgettable.
                   <ul>
                     <li>
-                      <strong>Founder:</strong> Michael W started the company in
-                      2024 with a vision to provide excellent audio for weddings
-                      and events, so no one has to suffer bad audio during their
-                      day. These memories will be for the record, and you want
-                      them to sound great.
+                      <strong>Founder:</strong> Michael Wegter started the
+                      company in 2024 with a vision to provide excellent audio
+                      for weddings and events.
                     </li>
                     <li>
                       <strong>DJ Experience:</strong> Michael has over 9 years
@@ -65,37 +64,33 @@ const About = () => {
             </Card>
           </Col>
         </Row>
-        <Row className="about-row">
-          <Col md={12} className="about-card-text">
+        <Row className="justify-content-center">
+          <Col md={8}>
             <Card className="about-card">
               <Card.Body>
                 <Card.Title>Our Journey</Card.Title>
                 <Card.Text>
-                  From humble beginnings to becoming a leading name in the DJ
-                  industry, our journey is a testament to our passion and
-                  dedication.
-                  <img
-                    src={AshleyAndMe}
-                    alt="Our Journey"
-                    className="embedded-photo"
-                  />
-                  <p>
-                    It all started in 2024 when Michael decided to turn his
-                    passion for audio and music into a professional service.
-                    Over the years, we have achieved numerous milestones.
-                  </p>
-                  <p>
-                    Today, we are proud to have served countless happy clients.
-                    Our mission is to provide unparalleled audio experiences for
-                    all events.
-                  </p>
+                  Michael started making music in elementary school, as a
+                  saxophonist at school and a music producer at home in 3rd
+                  grade when his brother introduced FL Studio to him. In
+                  college, in Shanghai, the club DJ atmosphere struck Michael,
+                  and he bought a DJ controller when he got back to the states.
+                  Now he's DJed dozens of events, and is excited to host yours!
+                  With an ear and a degree for music, and a passion for making
+                  great audio experiences, Michael wants your day to be sounding
+                  perfect.
                 </Card.Text>
+                <img
+                  src={AshleyAndMe}
+                  alt="Our Journey"
+                  className="embedded-photo"
+                />
               </Card.Body>
             </Card>
           </Col>
         </Row>
-        <Row className="about-row">
-          <Col md={12} className="about-card-text">
+        <Row className="justify-content-center">
+          <Col md={8}>
             <Card className="about-card">
               <Card.Img variant="top" src={DJDecks} alt="Our Events" />
               <Card.Body>

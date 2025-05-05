06e7e00a8055203aab791be27554b4204e2b61e4
mwegter95 <mwegter95@gmail.com>
2025-05-05 13:51:42 -0500

    homepage working nicely all screens

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
-import { Card, Button, Container, Row, Col } from "react-bootstrap";
+import { Card, Button, Container } from "react-bootstrap";
 import "./Home.scss";
 import PortraitMe from "../images/Portrait_me.jpeg";
 import WeddingDance from "../images/wedding_dance_bouqet_toss.jpg";
@@ -16,57 +16,52 @@ const Home = () => {
           Book Now!
         </Link>
       </div>
+
       <Container className="cards-container">
-        <Row>
-          <Col md={4}>
-            <Card className="info-card">
-              <div className="card-photo portrait">
-                <img src={PortraitMe} alt="About Us" />
-              </div>
-              <Card.Body>
-                <Card.Title>About Us</Card.Title>
-                <Card.Text>
-                  Learn more about For The Record and our journey.
-                </Card.Text>
-                <Button variant="primary" href="/about">
-                  Learn More
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="info-card">
-              <div className="card-photo bouquet">
-                <img src={WeddingDance} alt="Weddings" />
-              </div>
-              <Card.Body>
-                <Card.Title>Weddings</Card.Title>
-                <Card.Text>
-                  Make your special day unforgettable with our DJ services.
-                </Card.Text>
-                <Button variant="primary" href="#weddings">
-                  Learn More
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="info-card">
-              <div className="card-photo dj-decks">
-                <img src={DJDecks} alt="Events" />
-              </div>
-              <Card.Body>
-                <Card.Title>Events</Card.Title>
-                <Card.Text>
-                  We cater to all kinds of events, big or small.
-                </Card.Text>
-                <Button variant="primary" href="#events">
-                  Learn More
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <Card className="info-card">
+          <div className="card-photo portrait">
+            <img src={PortraitMe} alt="About Us" />
+          </div>
+          <Card.Body>
+            <Card.Title>About Us</Card.Title>
+            <Card.Text>
+              Learn more about For The Record and our journey.
+            </Card.Text>
+            <Button variant="primary" href="/about">
+              Learn More
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="info-card">
+          <div className="card-photo bouquet">
+            <img src={WeddingDance} alt="Weddings" />
+          </div>
+          <Card.Body>
+            <Card.Title>Weddings</Card.Title>
+            <Card.Text>
+              Make your special day unforgettable with our DJ services.
+            </Card.Text>
+            <Button variant="primary" href="#weddings">
+              Learn More
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card className="info-card">
+          <div className="card-photo dj-decks">
+            <img src={DJDecks} alt="Events" />
+          </div>
+          <Card.Body>
+            <Card.Title>Events</Card.Title>
+            <Card.Text>
+              We cater to all kinds of events, big or small.
+            </Card.Text>
+            <Button variant="primary" href="#events">
+              Learn More
+            </Button>
+          </Card.Body>
+        </Card>
       </Container>
     </div>
   );
